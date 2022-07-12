8582c74985dd557f9800e89d781ecba07a1aff76
chanooda <hanrhfqkq@gmail.com>
2022-07-12 18:07:00 +0900

    feat : api 접근 감추기
    next.config 파일 수정해서 api key 감추기

--- pages/api/movieApi.ts
+++ pages/api/movieApi.ts
@@ -1,8 +1,5 @@
-const API_KEY = "f6fbe9a13820afc7b29d03b000ebe095";
-const API_URL = "https://api.themoviedb.org/3/movie";
-
 export const getPopularMovies = async () => {
-  const response = await fetch(`${API_URL}/popular?api_key=${API_KEY}&page=1`);
+  const response = await fetch(`/api/movies`);
   const data = await response.json();
   return data.results;
 };
