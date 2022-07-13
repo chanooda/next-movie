443604b9f34927e32b6c869304caf8cb6400bd84
chanooda <hanrhfqkq@gmail.com>
2022-07-13 11:36:46 +0900

    detail page dynamic routes

--- components/movie.tsx
+++ components/movie.tsx
@@ -1,18 +1,21 @@
+import Link from "next/link";
 import { IMovieProps } from "../interfaces/movieInterface";
 import { MovieContainer, MovieTitle } from "./styled-components/movieStyled";
 
 export default function Movie({ movieInfo }: IMovieProps) {
   return (
     <>
-      <MovieContainer>
-        <div className="img_container">
-          <img
-            src={`https://image.tmdb.org/t/p/w200/${movieInfo.poster_path}`}
-            alt={movieInfo.title}
-          />
-        </div>
-        <MovieTitle>{movieInfo.title}</MovieTitle>
-      </MovieContainer>
+      <Link href={`/detail/${movieInfo.id}`}>
+        <MovieContainer>
+          <div className="img_container">
+            <img
+              src={`https://image.tmdb.org/t/p/w200/${movieInfo.poster_path}`}
+              alt={movieInfo.title}
+            />
+          </div>
+          <MovieTitle>{movieInfo.title}</MovieTitle>
+        </MovieContainer>
+      </Link>
     </>
   );
 }
